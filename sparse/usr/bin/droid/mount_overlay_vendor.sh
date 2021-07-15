c768e93eedf987357b94e9d057cf057d90126a76
BirdZhang <0312birdzhang@gmail.com>
2021-07-15 08:17:06 +0000

    hack for mount vendor-*

--- sparse/usr/bin/droid/mount_overlay_vendor.sh
+++ sparse/usr/bin/droid/mount_overlay_vendor.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 mount -t overlay overlay -o lowerdir=/usr/libexec/droid-hybris/vendor:/vendor /vendor
+
+# hack for vendor-*.mount, why sometime they not work?
+/bin/mount /dev/mmcblk0p67 /vendor/dsp -t ext4 -o ro,nosuid,nodev,barrier=1
+/bin/mount /dev/mmcblk0p80 /vendor/firmware_mnt -t vfat -o ro,shortname=lower,uid=0,gid=1000,dmask=227,fmask=337
+/bin/mount /dev/mmcblk0p36 /vendor/bt_firmware -t vfat -o ro,shortname=lower,uid=1002,gid=3002,dmask=227,fmask=337
