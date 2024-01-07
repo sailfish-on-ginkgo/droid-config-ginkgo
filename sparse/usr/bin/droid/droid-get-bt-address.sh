556c9e2ac9d4b49261339dc7b233aa0dd274dd6a
BirdZhang <0312birdzhang@gmail.com>
2024-01-07 17:28:06 +0800

    Update droid-get-bt-address.sh

--- sparse/usr/bin/droid/droid-get-bt-address.sh
+++ sparse/usr/bin/droid/droid-get-bt-address.sh
@@ -4,10 +4,17 @@ echo "droid-get-bt-address: Setting up bluetooth address"
 hexchars="0123456789ABCDEF"
 addresspath="/var/lib/bluetooth/"
 addressfile="$addresspath/board-address"
-count=10
-mac=$( for i in $(seq $count) ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/:\1/g' )
+
+# use real bluetooth address
+ls $addresspath|grep ':'
+if [ "$?" -eq "0" ]; then
+   ls $addresspath|grep ':'|head -n 1 > $addressfile
+   exit 0
+fi
 
 if [ ! -f "$addressfile" ]; then
+    count=10
+    mac=$( for i in $(seq $count) ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/:\1/g' )
     echo "File not found, generating new address"
     mkdir -p "$addresspath"
     chmod 0755 "$addresspath"
