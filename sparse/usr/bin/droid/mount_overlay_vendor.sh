36c6dc9a698040a50a23e7a51d93ec7592bf3c47
BirdZhang <0312birdzhang@gmail.com>
2024-07-14 16:18:37 +0800

    Update mount_overlay_vendor.sh

--- sparse/usr/bin/droid/mount_overlay_vendor.sh
+++ sparse/usr/bin/droid/mount_overlay_vendor.sh
@@ -2,16 +2,8 @@
 mount -t overlay overlay -o lowerdir=/usr/libexec/droid-hybris/vendor:/vendor /vendor ||:
 
 # hack for vendor-*.mount, why sometime they not work?
-if ! mount|grep -q /vendor/dsp; then
-  /bin/mount /dev/mmcblk0p67 /vendor/dsp -t ext4 -o ro,nosuid,nodev,barrier=1 ||:
-fi
-if ! mount|grep -q /vendor/firmware_mnt; then 
-  /bin/mount /dev/mmcblk0p80 /vendor/firmware_mnt -t vfat -o ro,shortname=lower,uid=0,gid=1000,dmask=227,fmask=337 ||:
-fi
-if ! mount|grep -q /vendor/bt_firmware; then
-  /bin/mount /dev/mmcblk0p36 /vendor/bt_firmware -t vfat -o ro,shortname=lower,uid=1002,gid=3002,dmask=227,fmask=337 ||:
-fi
-if ! mount|grep -q /mnt/vendor/persist; then
-  /bin/mount /dev/mmcblk0p69 /mnt/vendor/persist -t ext4 -o noatime,nosuid,nodev,barrier=1 ||:
-fi
+/bin/mount /dev/mmcblk0p67 /vendor/dsp -t ext4 -o ro,nosuid,nodev,barrier=1 ||:
+/bin/mount /dev/mmcblk0p80 /vendor/firmware_mnt -t vfat -o ro,shortname=lower,uid=0,gid=1000,dmask=227,fmask=337 ||:
+/bin/mount /dev/mmcblk0p36 /vendor/bt_firmware -t vfat -o ro,shortname=lower,uid=1002,gid=3002,dmask=227,fmask=337 ||:
+/bin/mount /dev/mmcblk0p69 /mnt/vendor/persist -t ext4 -o noatime,nosuid,nodev,barrier=1 ||:
 
